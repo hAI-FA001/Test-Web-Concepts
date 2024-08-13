d30e34d5634421df19ca542b8af2c1d6a5429f39
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-08-13 12:33:27 +0500

    log cookie

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -10,20 +10,20 @@ router.post("/signup", (req, res) => {
 router.post('/login', (req, res) => {
     req.session.user = new User(req.body.username ?? "default name", req.body.password ?? "default pass");
     console.log(req.session.user);
-    console.logo("Cookie is ", req.cookies)
+    console.log("Cookie is ", req.cookies)
     res.status(200).json("Logged In").send();
 });
 
 router.post('/logout', (req, res) => {
     req.session.user = null;
     console.log(req.session.user);
-    console.logo("Cookie is ", req.cookies)
+    console.log("Cookie is ", req.cookies)
     res.status(200).json("Logged Out").send();
 });
 
 router.get('/session', (req, res) => {
     console.log(req.session.user);
-    console.logo("Cookie is ", req.cookies)
+    console.log("Cookie is ", req.cookies)
     res.status(200).send("Session is: " + JSON.stringify(req.session));
 })
 
