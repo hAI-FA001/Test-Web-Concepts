5fe92024b30ed6b49981220baaf4885fb854ea28
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-08-13 12:38:12 +0500

    log SID

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -11,6 +11,7 @@ router.post('/login', (req, res) => {
     req.session.user = new User(req.body.username ?? "default name", req.body.password ?? "default pass");
     console.log(req.session.user);
     console.log("Cookie is ", req.cookies)
+    console.log("SID is ", req.sessionID)
     res.status(200).json("Logged In").send();
 });
 
@@ -18,12 +19,14 @@ router.post('/logout', (req, res) => {
     req.session.user = null;
     console.log(req.session.user);
     console.log("Cookie is ", req.cookies)
+    console.log("SID is ", req.sessionID)
     res.status(200).json("Logged Out").send();
 });
 
 router.get('/session', (req, res) => {
     console.log(req.session.user);
     console.log("Cookie is ", req.cookies)
+    console.log("SID is ", req.sessionID)
     res.status(200).send("Session is: " + JSON.stringify(req.session));
 })
 
