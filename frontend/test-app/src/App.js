fe98bb2c8d2d8f5ba0dfea48a06f078fb0cedc22
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-08-13 11:14:26 +0500

    basic frontend to play with server endpoints

---
+++ frontend/test-app/src/App.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const App = () => {
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [apiResponse, setApiResponse] = useState('');
+
+    function onSendLoginRequest(event) {
+        fetch(process.env.REACT_APP_SERVER_BASE_URL + "/user/login",
+            {
+                method: "POST",
+                body: JSON.stringify({ username: username, password: password }),
+                headers: { "Content-Type": "application/json; charset=UTF-8" },
+            }
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                setApiResponse(data);
+            });
+        }
+        
+        function onSendLogoutRequest(event) {
+            fetch(process.env.REACT_APP_SERVER_BASE_URL + "/user/logout",
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json; charset=UTF-8" }
+            }
+            )
+            .then((res) => res.json())
+            .then((data) => {
+                setApiResponse(data);
+            });
+    }
+
+    return (
+        <div className="main-container">
+            <h1>Send a Request to Server</h1>
+            <div className="middle-section">
+                <input onInput={e => setUserName(e.target.value)} value={username} name="username" placeholder="User Name" type="text" />
+                <input onInput={e => setPassword(e.target.value)} value={password} name="password" placeholder="Password" type="password" />
+                <div className="btns-container">
+                    <button onClick={onSendLoginRequest}>Login Request</button>
+                    <button onClick={onSendLogoutRequest}>Logout Request</button>
+                </div>
+                <div><p>{apiResponse? apiResponse : "Response will appear here"}</p></div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
